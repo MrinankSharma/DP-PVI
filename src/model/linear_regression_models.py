f3cd33bf7a445ff811b93221a813e9f6bb6f76fd
Xiping Liu <xl2639@columbia.edu>
2019-07-28 23:58:44 +0100

    implement predict method in the torch class and start with the wrapper class

--- src/model/linear_regression_models.py
+++ src/model/linear_regression_models.py
@@ -144,12 +144,12 @@ class LinearRegressionTorchModule(nn.Module):
             self.prior_mu = torch.tensor(prior_params["w_mu"], dtype=torch.float32)
             self.prior_log_var = torch.tensor(prior_params["w_log_var"], dtype=torch.float32)
 
-    def predict(self, x, integration_limit=500, parameters=None):
+    def predict(self, x, parameters=None):
         """
         Bayesian prediction on probability at certain point, performed using numerical integration
         so this is the 'fully Bayesian' probability
 
-        :param x: data to predict on
+        :param x: a single data point to predict on
         :param integration_limit: how much to truncate numerical integration for the predictive distribution
         :param parameters: logistic regression parameters
         :param hyperparameters: logistic regression hyperparams
@@ -157,18 +157,11 @@ class LinearRegressionTorchModule(nn.Module):
         """
 
         self.set_parameters(parameters)
-        # all per point
-        mean_1d = torch.mv(x, self.w_mu)
-        cov_mat = torch.diag(torch.exp(self.w_log_var))
-        cov_list = torch.diag(torch.mm(x, torch.mm(cov_mat, x.t())))
-        mean_vars = list(zip(mean_1d, cov_list))
-        p_vals = torch.Tensor(len(mean_vars))
+        
+        y_mean = x @ self.w_mu.data
+        y_var = (x ** 2) @ torch.exp(self.w_log_var.data) + self.noise ** 2
 
-        for ind, mean_var in enumerate(mean_vars):
-            p_val, err = quad(prediction_function, -integration_limit, integration_limit, mean_var)
-            p_vals[ind] = p_val
-
-        return p_vals
+        return y_mean, y_var
 
     def forward(self, x, parameters=None):
         """
@@ -228,3 +221,136 @@ class LinearRegressionTorchModule(nn.Module):
         w_mu = parameters["w_mu"]
         
         return torch.mv(x, w_mu) + random.normal(0, self.noise, x.shape[0])
+
+
+class LinearRegressionMultiDimSGD(Model):
+
+    def __init__(self, parameters, hyperparameters):
+        # initialise torch module first
+        self.torch_module = LinearRegressionTorchModule(nat_params_to_params_dict(parameters), hyperparameters)
+        super(LinearRegressionMultiDimSGD, self).__init__(parameters, hyperparameters)
+
+        base_optimizer = self.hyperparameters['base_optimizer_class'](self.torch_module.parameters(),
+                                                                      **hyperparameters['base_optimizer_parameters'])
+
+        self.wrapped_optimizer = self.hyperparameters['wrapped_optimizer_class'](optimizer=base_optimizer,
+                                                                                 model=self.torch_module,
+                                                                                 loss_per_example=self.torch_module.compute_loss_per_point,
+                                                                                 **hyperparameters[
+                                                                                     'wrapped_optimizer_parameters'])
+
+        self._training_curves = []
+        self._derived_statistics_histories = []
+
+    def set_hyperparameters(self, hyperparameters):
+        if hyperparameters is not None:
+            self.hyperparameters = hyperparameters
+
+    def set_parameters(self, nat_parameters):
+        if nat_parameters is not None:
+            parameters = nat_params_to_params_dict(nat_parameters)
+            self.torch_module.set_parameters_from_numpy(parameters)
+
+    def get_parameters(self):
+        return params_to_nat_params_dict({
+            'w_mu': self.torch_module.w_mu.detach().numpy(),
+            'w_log_var': self.torch_module.w_log_var.detach().numpy()
+        })
+
+    def get_default_parameters(self):
+        return {
+            'w_pres': 1,
+            'w_nat_mean': 0
+        }
+
+    def get_default_hyperparameters(self):
+        return {
+            "base_optimizer_class": None,
+            "wrapped_optimizer_class": None,
+            "base_optimizer_params": {},
+            "wrapped_optimizer_params": {},
+            "N_steps": 1,
+            "n_in": 2,
+            "model_noise": 0    # model_noise is standard deviation
+        }
+
+    def sample(self, x, parameters):
+        """
+        Create some logistic regression data. *** NOTE: This ignores the precisions of each of the values of w, and
+        simply assuming the true (unknown) weight is w; this is different to finding the predictive distribution!! ***
+
+        :param x: input values to predict at
+        :param parameters: model parameters (not will not update)
+        :param hyperparameters: model hyperparameters (will also not update)
+        :return: y: tensor of parameter labels
+        """
+        return self.torch_module.sample(x, parameters).numpy()
+
+    def predict(self, x, parameters=None, hyperparameters=None):
+        """
+        Bayesian prediction on probability at certain point, performed using numerical integration
+        so this is the 'fully Bayesian' probability
+
+        :param x: data to predict on
+        :param integration_limit: how much to truncate numerical integration for the predictive distribution
+        :param parameters: logistic regression parameters
+        :param hyperparameters: logistic regression hyperparams
+        :return: probability of each point in x being +1
+        """
+        super().predict(x, parameters, hyperparameters)
+
+        return self.torch_module.predict(x, parameters).numpy()
+
+    def fit(self, data, t_i, parameters=None, hyperparameters=None):
+        """
+        Note that this method will start the gradient optimisation from the current parameter values
+
+        :param data:
+        :param t_i:
+        :param parameters:
+        :param hyperparameters:
+        :return: optimum params
+        """
+        super().fit(data, t_i, parameters, hyperparameters)
+
+        # convert data into a tensor
+        x = torch.tensor(data["x"], dtype=torch.float32)
+        y_true = torch.tensor(data["y"], dtype=torch.float32)
+
+        cav_nat_params = B.subtract_params(self.get_parameters(), t_i)
+        # numpy dict for the effective prior
+        self.torch_module.set_prior_parameters_from_numpy(cav_nat_params)
+
+        print_interval = np.ceil(self.hyperparameters['N_steps'] / 20)
+
+        training_curve = np.empty(self.hyperparameters['N_steps'])
+        derived_statistics_history = []
+
+        self._derived_statistics_history = []
+        # lets just do this for the time being
+        for i in range(self.hyperparameters['N_steps']):
+            current_loss = self.wrapped_optimizer.fit_batch(x, y_true)
+            derived_statistics = self.wrapped_optimizer.get_logged_statistics()
+            derived_statistics_history.append(derived_statistics)
+            training_curve[i] = current_loss
+            if i % print_interval == 0:
+                logger.info("Loss: {:.3f} after {} steps".format(current_loss, i))
+
+        # if several fit batches are called, this puts all of their training curves into a list
+        self._training_curves.append(training_curve)
+        self._derived_statistics_histories.append(derived_statistics_history)
+
+        return self.get_parameters()
+
+    def get_incremental_log_record(self):
+        ret = {
+            "derived_statistics": self._derived_statistics_histories,
+            "training_curves": self._training_curves,
+        }
+        self._training_curves = []
+        self._derived_statistics_histories = []
+        return ret
+
+    def get_incremental_sacred_record(self):
+        # we don't want anything from the model to be displayed directly to sacred
+        return {}
