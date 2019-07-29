6b99de157a963eec944d7c5b7cb1d11451cc5df6
Xiping Liu <xl2639@columbia.edu>
2019-07-29 15:05:06 +0100

    modify analytical classes so that the notations are consistent

--- src/model/linear_regression_models.py
+++ src/model/linear_regression_models.py
@@ -12,8 +12,8 @@ class LinearRegression1DAnalyticNumpy(Model):
 
     def get_default_parameters(self):
         return {
-            'w_nat_mean': np.zeros(1),    # natural mean = mu / sigma squared
-            'w_precision': np.zeros(1)    # precision = -1 / (2 * sigma squared)
+            'w_nat_mean': 0,    # natural mean = mean / var
+            'w_pres': 1         # precision = 1 / var
         }
 
     def get_default_hyperparameters(self):
@@ -36,8 +36,8 @@ class LinearRegression1DAnalyticNumpy(Model):
                 self.parameters,
                 t_i),
             {
-                'w_nat_mean': x.T @ y / (self.noise ** 2),
-                'w_precision': x.T @ x / (-2 * self.noise ** 2)
+                'w_nat_mean': (x.T @ y) / self.noise ** 2,
+                'w_pres': (x.T @ x) / self.noise ** 2
             }
         )
 
@@ -45,13 +45,13 @@ class LinearRegression1DAnalyticNumpy(Model):
         super().predict(x, parameters, hyperparameters)
 
     def sample(self, x, parameters=None, hyperparameters=None):
-        eta_1 = self.parameters['w_nat_mean']
-        eta_2 = self.parameters['w_precision']
+        nat_mean = self.parameters['w_nat_mean']
+        pres = self.parameters['w_pres']
 
-        mu = -eta_1 / (2 * eta_2)
-        sigma_squared = -1 / (2 * eta_2)
+        mean = nat_mean / pres
+        var = 1 / pres
 
-        return x * mu + random.normal(0, self.noise, x.shape)
+        return x * mean + random.normal(0, self.noise, x.shape)
 
     def get_incremental_sacred_record(self):
         return {}
@@ -64,19 +64,19 @@ class LinearRegressionMultiDimAnalyticNumpy(Model):
 
     def get_default_parameters(self):
         return {
-            'w_nat_mean': np.zeros((2, 1)),    # natural mean = sigma inverse * mu
-            'w_precision': np.zeros((2, 2))    # precision = sigma inverse / -2
+            'w_nat_mean': np.zeros(2),    # natural mean = var inverse * mu
+            'w_pres': np.ones(2)          # precision = var inverse
         }
 
     def get_default_hyperparameters(self):
         return {
-            'dimension': 2,
+            'n_in': 2,
             'model_noise': 0    # model_noise is standard deviation
         }
 
     def set_hyperparameters(self, hyperparameters):
         super().set_hyperparameters(hyperparameters)
-        self.dim = self.hyperparameters['dimension']
+        self.n_in = self.hyperparameters['n_in']
         self.noise = self.hyperparameters['model_noise']
 
     def fit(self, data, t_i, parameters=None, hyperparameters=None):
@@ -90,8 +90,8 @@ class LinearRegressionMultiDimAnalyticNumpy(Model):
                 self.parameters,
                 t_i),
             {
-                'w_nat_mean': x.T @ y / (self.noise ** 2),
-                'w_precision': np.diag(np.diag(x.T @ x)) / (-2 * self.noise ** 2)
+                'w_nat_mean': (x.T @ y) / self.noise ** 2,
+                'w_pres': np.diag(x.T @ x) / self.noise ** 2
             }
         )
 
@@ -99,13 +99,13 @@ class LinearRegressionMultiDimAnalyticNumpy(Model):
         super().predict(x, parameters, hyperparameters)
 
     def sample(self, x, parameters=None, hyperparameters=None):
-        eta_1 = self.parameters['w_nat_mean']
-        eta_2 = self.parameters['w_precision']
+        nat_mean = self.parameters['w_nat_mean']
+        pres = self.parameters['w_pres']
 
-        mu = np.linalg.inv(eta_2) @ eta_1 / -2
-        sigma = np.linalg.inv(eta_2) / -2
+        mean = nat_mean / pres
+        var = 1 / pres
 
-        return x @ mu + random.normal(0, self.noise, (x.shape[0], 1))
+        return x @ mean + random.normal(0, self.noise, x.shape[0])
 
     def get_incremental_sacred_record(self):
         return {}
