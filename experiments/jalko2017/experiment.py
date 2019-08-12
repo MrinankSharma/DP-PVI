17eed48cc8b01bbff6ae18e23e8453707e4cde19
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2019-08-12 11:15:44 +0100

    update config file

--- experiments/jalko2017/experiment.py
+++ experiments/jalko2017/experiment.py
@@ -52,7 +52,7 @@ def default_config(dataset):
     elif dataset["name"] == "adult":
         privacy_settings = {
             "L": 195,
-            "C": 75,
+            "C": 7.5,
             "target_delta": 1e-3,
             "sigma_relative": 1.22
         }
