0ced429d8462c8c47c130eb408ae85cad3ba2eac
mjhutchinson <hutchinson.michael.john@gmail.com>
2019-08-22 11:46:31 +0100

    Simple addition to correctly add execution directory to system path of the python process

--- experiments/dispatch/simple_dispatcher.py
+++ experiments/dispatch/simple_dispatcher.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+module_path = os.path.abspath(os.path.join('.'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+print(module_path)
+print(sys.path)
+
 import argparse
 import logging
 from subprocess import call
