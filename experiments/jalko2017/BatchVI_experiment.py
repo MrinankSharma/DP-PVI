c0e8491d6b42461ffaa937a427da4a336f28f7fc
mjhutchinson <hutchinson.michael.john@gmail.com>
2019-09-03 18:35:39 +0100

    trying sleep fix to mongo logging issue

--- experiments/jalko2017/BatchVI_experiment.py
+++ experiments/jalko2017/BatchVI_experiment.py
@@ -119,11 +119,11 @@ def run_experiment(ray_cfg,
         training_set, test_set, d_in = load_data()
         clients_data, nis, prop_positive, M = generate_dataset_distribution_func()(training_set["x"], training_set["y"])
 
-        _run.info = {
-            **_run.info,
-            "prop_positive": prop_positive,
-            "n_is": nis,
-        }
+        # _run.info = {
+        #     **_run.info,
+        #     "prop_positive": prop_positive,
+        #     "n_is": nis,
+        # }
 
         logger.info(f'N_i\'s {nis}')
         logger.info(f'Class ratios\'s {prop_positive}')
